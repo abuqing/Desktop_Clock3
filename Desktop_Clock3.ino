8da17cb95f2b6b4cad17c9a877e8a79b7a6580e5
Thomas <47178539+kiyoakiabe@users.noreply.github.com>
2019-03-30 23:17:12 +0800

    add color code and disable serial comm

--- Desktop_Clock3.ino
+++ Desktop_Clock3.ino
@@ -3,7 +3,7 @@
 Wemos D1 mini
 
 Touch sensor
-GPIO 0 (D3) attachInterrupt
+GPIO 3 (RX) attachInterrupt
 
 CDS sensor
 ADC0 A0 Pin
@@ -29,162 +29,152 @@ https://github.com/adafruit/Adafruit-GFX-Library
 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
-#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+//#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
 #include <Wire.h>
 #include <DS3231.h>
 #include <Ticker.h>
+
+#include <Fonts/FreeSansBold24pt7b.h>
+#include <Fonts/FreeSansBold18pt7b.h>
+#include <Fonts/FreeSansBold12pt7b.h>
+#include <Fonts/FreeSansBold9pt7b.h>
 
 #define TFT_PIN_CS   15
 #define TFT_PIN_DC   2
 #define TFT_PIN_RST  12
 #define TFT_BACKLIGHT  16 // Display backlight pin
 
+//Font Color Settings
+#define SLATEGRAY 0x634c
+#define GAINSBORO 0xdedb
+#define CRIMSON 0xd887
+#define ORANGERED 0xfa20
+#define LIMEGREEN 0x2e65
+#define DEEPSKYBLUE 0x05ff
+#define LIGHTBLUE 0xaebc
+#define AZURE 0xefff
+
+
 volatile int state = HIGH;
 Ticker ticker;
 volatile int state2 = HIGH;
 
-int sig_pin = 0; // Touch sensor
+int sig_pin = 3; // Touch sensor
 int cdsVal = 0;
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_PIN_CS, TFT_PIN_DC, TFT_PIN_RST);
 
 
-float p = 3.1415926;
-char deg_sym = (char) 176;
-
 DS3231 clock;
 RTCDateTime dt;
 
 void setup(void) {
-  Serial.begin(9600);
+//  Serial.begin(9600);
+//  Serial.println("Desktop Clock service start");
   pinMode(sig_pin,INPUT);
-  attachInterrupt(sig_pin,displayOnOff,RISING);
-  ticker.attach(30, readTempTimer);
+  
   // Initializer if using a 1.8" TFT screen:
   pinMode(TFT_BACKLIGHT, OUTPUT);
   digitalWrite(TFT_BACKLIGHT, state); // Backlight on
   tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
-  Serial.println(F("1.8 TFT screen Initialized"));
+//  Serial.println(F("1.8 TFT screen Initialized"));
   tft.setRotation(1); //Rotate Display 0=0, 1=90, 2=180, 3=240
   tft.fillScreen(ST77XX_BLACK);
  
   // Initialize DS3231
-  Serial.println("Initialize DS3231");
+//  Serial.println("Initialize DS3231");
   clock.begin();
   // Set sketch compiling time
 //clock.setDateTime(__DATE__, __TIME__);
+  ticker.attach(30, readTempTimer);
+  
+//  Serial.println("Initialize Touch Sensor");
+  attachInterrupt(sig_pin,displayOnOff,RISING);
+    
 }
 
 void loop() {
-  Serial.print("backlight state = ");
-  Serial.println(state);
-  
+  //  Serial.print("backlight state = ");
+  //  Serial.println(state);
   digitalWrite(TFT_BACKLIGHT, state);
-  delay(1000);
-
-
+    
   if (state2){
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.drawRoundRect(15,35,135,40,10,ST77XX_WHITE);
+    tft.fillScreen(ST77XX_BLACK);
+//    tft.setTextColor(ST77XX_WHITE);
+    tft.setTextColor(GAINSBORO);
+    
+    tft.drawRoundRect(15,35,135,40,10,GAINSBORO);
+    tft.setTextSize(0);
+    
+    dt = clock.getDateTime();
+    tft.setFont(&FreeSansBold12pt7b);
+    tft.setCursor(5, 25);
+    tft.print(dt.year);
+    tft.print('/');
+    tft.print(dt.month);
+    tft.print('/');
+    tft.print(dt.day);
   
-  dt = clock.getDateTime();
-  // For leading zero look to DS3231_dateformat example
-  Serial.print("Raw data: ");
-  Serial.print(dt.year);   Serial.print("-");
-  Serial.print(dt.month);  Serial.print("-");
-  Serial.print(dt.day);    Serial.print(" ");
-  Serial.print(dt.hour);   Serial.print(":");
-  Serial.print(dt.minute); Serial.print(":");
-  Serial.print(dt.second); Serial.println("");
-   
-  Serial.print("Temperature: "); 
-  Serial.print(clock.readTemperature());
-  Serial.println(" *C ");
-
-  tft.setCursor(0, 0);
-  tft.setTextSize(2);
-  tft.print(dt.year);
-  tft.print('/');
-  tft.print(dt.month);
-  tft.print('/');
-  tft.print(dt.day);
-
-  tft.setCursor(20, 120);
-  tft.setTextSize(1);
-  tft.print("Temperature : ");
-  tft.print(clock.readTemperature());
-//  tft.print(deg_sym);
-  tft.println(" C");
+    tft.setFont(&FreeSansBold24pt7b);
+    tft.setCursor(10,70);
+    tft.print(" ");
+    tft.print(dt.hour);
+    tft.print(':');
+    tft.print(dt.minute);
   
-  tft.setCursor(0,40);
-  tft.setTextSize(4);
-  tft.print(" ");
-  tft.print(dt.hour);
-  tft.print(':');
-  tft.print(dt.minute);
+    tft.setFont(&FreeSansBold9pt7b);
+    tft.setCursor(20, 110);
+    tft.print("Temp : ");
+    tft.print(clock.readTemperature());
+    tft.println(" C");
+    
+    cdsVal = analogRead(A0); // input CDS sensor value
 
-  cdsVal = analogRead(A0); // input CDS sensor value
-  Serial.print("Lighting value : "); 
-  Serial.print(cdsVal);
-  tft.setCursor(20, 110);
-  tft.setTextSize(1);
-  tft.print("Lighting value : ");
-  tft.print(cdsVal);
+    tft.setCursor(20, 125);
+    tft.print("Lighting : ");
+    tft.print(cdsVal);
+  
+    tft.setCursor(20, 95);
+    if (clock.readTemperature() > 35){
+       tft.setTextColor(CRIMSON);
+       tft.print("   Hot !!!");
+    } else if (clock.readTemperature() > 29){
+       tft.setTextColor(ORANGERED);
+       tft.print("    Warm");
+    } else if (clock.readTemperature() > 21){
+        tft.setTextColor(LIMEGREEN);
+        tft.print(" Comfortable");
+    } else if (clock.readTemperature() > 15){
+//        tft.setTextColor(ST77XX_BLUE);
+        tft.setTextColor(DEEPSKYBLUE);
+        tft.print("     Cool");
+    } else if (clock.readTemperature() > 1){
+        tft.setTextColor(LIGHTBLUE);
+        tft.print("     Cold");
+    } else {      
+        tft.setTextColor(AZURE);
+        tft.print("   Freezing");
+    }
 
-  tft.setCursor(0, 80);
-  tft.setTextSize(2);
-  if (clock.readTemperature() > 35){
-     tft.setTextColor(ST77XX_RED);
-     tft.print("   Hot !!!");
-  } else if (clock.readTemperature() > 29){
-     tft.setTextColor(ST77XX_RED);
-     tft.print("    Warm");
-  } else if (clock.readTemperature() > 21){
-      tft.setTextColor(ST77XX_GREEN);
-      tft.print(" Comfortable");
-  } else if (clock.readTemperature() > 15){
-      tft.setTextColor(ST77XX_BLUE);
-      tft.print("     Cool");
-  } else if (clock.readTemperature() > 1){
-      tft.setTextColor(ST77XX_BLUE);
-      tft.print("     Cold");
-  } else {      
-      tft.setTextColor(ST77XX_BLUE);
-      tft.print("   Freezing");
-  }
+//    Serial.print("Raw data: ");
+//    Serial.print(dt.year);   Serial.print("-");
+//    Serial.print(dt.month);  Serial.print("-");
+//    Serial.print(dt.day);    Serial.print(" ");
+//    Serial.print(dt.hour);   Serial.print(":");
+//    Serial.print(dt.minute); Serial.print(":");
+//    Serial.print(dt.second); Serial.println("");
+//    Serial.print("Temperature: "); 
+//    Serial.print(clock.readTemperature());
+//    Serial.println(" *C ");
+//    Serial.print("Lighting value : "); 
+//    Serial.println(cdsVal);  
   
-  state2 = !state2;
-  }
-  
-// Serial.println(digitalRead(sig_pin));
-//  if(digitalRead(sig_pin))
-//  {
-//
-//     tft.setCursor(0, 80);
-//     tft.setTextColor(ST77XX_RED);
-//     tft.setTextSize(2);
-//     tft.print("Switch ON ! ");
-//     displayOnOff();
-//     digitalWrite(TFT_BACKLIGHT, state); // Backlight on
-//     
-//  }
-//  else
-//  {
-//
-//     tft.setCursor(0, 80);
-//     tft.setTextColor(ST77XX_GREEN);
-//     tft.setTextSize(2);
-//     tft.print("Switch OFF");
-//
-//    }
+    state2 = !state2;
+    }
 
-  
-//  tft.invertDisplay(true);
-//  delay(500);
-//  tft.invertDisplay(false);
-//  delay(500);
+
+  delay(100);
 }
 
 void displayOnOff()
